c9351812fa14f9849cd09ff5e5e8a7e70a38e37e
HS6103 <hsuchen1022@gmail.com>
2024-03-01 17:17:10 +0800

    modified intent tool, generated intent list

--- Toolkit/intent_generator.py
+++ Toolkit/intent_generator.py
@@ -8,8 +8,35 @@ with open("account.info", encoding="utf-8") as f:
     accountDICT = json.load(f)
 articut = Articut(username=accountDICT["username"], apikey=accountDICT["api_key"])
 
-with open ("../purged_corpus/中研院_須_purged.txt", mode="r", encoding="utf-8") as p:
-    inputSTR = p.read()
-    resultDICT = articut.parse(inputSTR, level='lv1')
-    verbStemLIST = articut.getVerbStemLIST(resultDICT)
-    pprint(verbStemLIST)    
+def listVerb(targetSTR):
+    
+    with open ("../purged_corpus/中研院_{}_purged.txt".format(targetSTR), mode="r", encoding="utf-8") as p:
+        print("Generating verbStemLIST...\n" + "="*60)        
+        inputSTR = p.read()
+        resultDICT = articut.parse(inputSTR)
+        verbStemLIST = articut.getVerbStemLIST(resultDICT)
+        print("\nFinished loading verbStemLIST...\n" + "="*60)
+    
+    with open('../purged_corpus/中研院_{}_intent.txt'.format(targetSTR),'a',encoding="utf-8") as g: # 將 corpusLIST 中的句子寫入 _purged.txt 檔
+        try:
+            for content in verbStemLIST:
+                if content != []:
+                    for entry in content:
+                        g.write(entry[2] + "\n")
+                else:
+                    pass
+            print("\nFinished")
+            
+        except Exception as e:
+            print("\nFailed!\n" + "The following error occurred: {}".format(e))
+    
+
+
+#entry_point
+if __name__ == "__main__":
+        
+    targetSTR = input("Enter target character: ")
+    print("\nRunning...")
+    
+    listVerb(targetSTR)
+    
