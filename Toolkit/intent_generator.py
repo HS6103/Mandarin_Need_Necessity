2fc7de9b31a8a73d3e7b16d401d7bdfb4bbea5fb
HS6103 <hsuchen1022@gmail.com>
2024-03-02 12:55:14 +0800

    modified intent tool comment

--- Toolkit/intent_generator.py
+++ Toolkit/intent_generator.py
@@ -20,12 +20,12 @@ def listVerb(targetSTR):
         verbStemLIST = articut.getVerbStemLIST(resultDICT)
         print("\nFinished loading verbStemLIST...\n" + "="*60)
     
-    with open('../purged_corpus/中研院_{}_intent.txt'.format(targetSTR),'a',encoding="utf-8") as g: # 將選取檔案中utterance的動詞寫入{}_intent.txt
+    with open('../purged_corpus/中研院_{}_intent.txt'.format(targetSTR),'a',encoding="utf-8") as g: # 將選取檔案中utterance的所有verbStem寫入{}_intent.txt
         try:
-            for entry in verbStemLIST:
-                if entry != []:
-                    for subentry in entry:
-                        g.write(subentry[2] + "\n")
+            for entry in verbStemLIST:  #撥開第一層list
+                if entry != []:  
+                    for verb in entry:           #撥開第二層list
+                        g.write(verb[2] + "\n")        #將不是empty list的verbStem寫入{}_intent.txt
                 else:
                     pass
             print("\nFinished\n")
@@ -33,8 +33,8 @@ def listVerb(targetSTR):
         except Exception as e:
             print("\nFailed!\n" + "The following error occurred: {}".format(e))
     
+
 def create_intent(targetSTR):
-        
         try:
             if targetSTR == '須':
                 project = 'deontic'
@@ -44,34 +44,33 @@ def create_intent(targetSTR):
             with open('../purged_corpus/中研院_{}_intent.txt'.format(targetSTR),'r',encoding="utf-8") as g:
                 print("Generating intent...\n" + "="*60)
                 for word in g:
-                    iLIST = articut.parse(word, level="lv3", pinyin="HANYU")["utterance"]
+                    iLIST = articut.parse(word, level="lv3", pinyin="HANYU")["utterance"]  #將文件內容(動詞)轉成拼音
                     intentSTR = iLIST[0].replace("/", "").replace(" ", "").strip()
                     
-                    url = "https://api.droidtown.co/Loki/Call/" #LokiCall Docker 版請自訂 URL
+                    url = "https://api.droidtown.co/Loki/Call/" 
                     payload = {
-                        "username" : accountDICT["username"], # 這裡填入您在 https://api.droidtown.co 使用的帳號 email。     Docker 版不需要此參數！
-                        "loki_key" : accountDICT["loki_key_{}".format(project)], # 這裡填入您在 https://api.droidtown.co 登入後取得的 loki_key。 Docker 版不需要此參數！
-                        "project": "{}_xu".format(project), #專案名稱 (請先在 Loki 的設定網頁裡建立一個 Project 以便取得它的專案金鑰 (loki_key)
+                        "username" : accountDICT["username"], 
+                        "loki_key" : accountDICT["loki_key_{}".format(project)], 
+                        "project": "{}_xu".format(project), 
                         "intent": "{}".format(intentSTR), #意圖名稱
                         "func": "create_intent",
                         "data": {
                             "type": "advance" #意圖類別
                         }
                     }
-                    print("Creating {}...".format(intentSTR))
+                    print("Creating {}...".format(intentSTR))  #建立意圖
                     response = post(url, json=payload).json()
                     pprint(response)
                     sleep(0.3)
-                    
             print("\nFinished\n")
             
         except Exception as e:
             print("\nFailed!\n" + "The following error occurred: {}".format(e))
             
+            
 #entry_point
 if __name__ == "__main__":
         
-    #targetSTR = input("Enter target character: ") #輸入要處理的字
     print("\nRunning...")
                     
     #listVerb("須")
