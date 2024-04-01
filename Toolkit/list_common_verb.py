9f88f68253f7202e2bb554b4a5e6beed7fd19d64
HS6103 <hsuchen1022@gmail.com>
2024-04-01 12:01:29 +0800

    modified tool, added unique intent list for both characters

--- Toolkit/list_common_verb.py
+++ Toolkit/list_common_verb.py
@@ -16,5 +16,32 @@ for intent in deonticLIST:
         pass
         
 with open('../purged_corpus/common_intent.txt','w',encoding="utf-8") as n:
+    cnt = 0
+    for intent in deonticLIST:
+        if (intent in epistemicLIST) and (intent not in common_intentLIST):
+            common_intentLIST.append(intent)
+            cnt += 1
+        else:
+            pass
     for entry in common_intentLIST:
         n.write(entry)
+
+with open('../purged_corpus/deontic_unique_intent.txt','a',encoding="utf-8") as deontic_u:
+    written_list = []
+    cnt = 0
+    for verb in deonticLIST:
+        if verb not in common_intentLIST and verb not in written_list:
+            deontic_u.write(verb)
+            written_list.append(verb)
+            cnt += 1
+    deontic_u.write("Total Count: {}".format(cnt))
+            
+with open('../purged_corpus/epistemic_unique_intent.txt','a',encoding="utf-8") as epistemic_u:
+    written_list = []
+    cnt = 0    
+    for verb in epistemicLIST:
+        if verb not in common_intentLIST and verb not in written_list:
+            epistemic_u.write(verb)
+            written_list.append(verb)
+            cnt += 1
+    epistemic_u.write("Total Count: {}".format(cnt))
